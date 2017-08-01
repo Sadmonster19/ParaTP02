b66f79a071aec3e87f9c8743f9ded5e536e5ff34
Sadmonster19 <simon.adam300@gmail.com>
2017-08-01 12:13:00 -0400

    Simple MPI project

---
+++ TP02/Readme.txt
@@ -0,0 +1,9 @@
+Readme
+
+Pour installer MPI, aller chercher MSMPI sur le site:
+https://msdn.microsoft.com/en-us/library/bb524831(v=vs.85).aspx
+
+Downloadez et runnez le MSI et le exe de la v8.1.
+Restart computer.
+Pour savoir si vous avez bien, allez dans CMD et runner la commande "set MSMPI".
+Si 4 lignes apparaissent vous êtes OK.  
--- TP02/TP02/Main.cpp
+++ TP02/TP02/Main.cpp
@@ -1,5 +1,24 @@
+#include "mpi.h"
 #include <iostream>
 
-int main() {
 
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        char helloStr[] = "Hello world";
+        MPI_Send(helloStr, _countof(helloStr), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+    }
+    else if (rank == 1) {
+        char helloStr[12];
+        MPI_Recv(helloStr, _countof(helloStr), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+        printf("Rank 1 received string %s from Rank 0\n", helloStr);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
